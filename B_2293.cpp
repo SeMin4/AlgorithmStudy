9a43639c44a25bbfd09a1a483b9f507ab00b4ee7
SeMin <ohsemin4@gmail.com>
2020-05-07 21:13:12 +0900

    C++ Algorithm Study wtih Baekjoon

---
+++ B_2293.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+int main() {
+    int n, k;
+    int coins[101];
+    int d[10001] = {0};
+
+    scanf("%d %d", &n, &k);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &coins[i]);
+    }
+
+    d[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            if (j >= coins[i]) {
+                d[j] += d[j-coins[i]];
+            }
+        }
+    }
+
+    printf("%d\n", d[k]);
+
+    return 0;
+}
