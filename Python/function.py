f698482126362b9e95055b39d48fda7fb0758d73
SeMin <ohsemin4@gmail.com>
2021-06-19 22:11:26 +0900

    Algorithm study wtih python

---
+++ Python/function.py
@@ -0,0 +1,31 @@
+def name(parameter1, parameter2, parameter3):
+    parameter1 = 3
+    return parameter1
+
+
+# print(pow(2, 3))
+# a = 3
+# if a is None:
+#     print("ABC")
+
+
+# flag = True
+# a = list()
+
+# a.append(1)
+# while a:
+#     c = a.pop()
+
+def function(a):
+    a[0] = 3
+
+a = [1,2,3]
+function(a)
+print(a[0])
+
+# 재귀 깊이 설정
+
+import sys
+sys.setrecursionlimit(10**9)
+
+
---
+++ Python/queue.py
@@ -0,0 +1,8 @@
+from collections import deque
+#deck 
+q = deque()
+# q.append(1)
+q.append(6)
+q.append(1)
+print(q.popleft())
+
